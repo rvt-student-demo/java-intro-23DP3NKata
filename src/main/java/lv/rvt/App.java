f64c4678751c151eaa335c75e22d4dcfdec65e92
23DP3NKata <166131686+23DP3NKata@users.noreply.github.com>
2024-10-01 13:17:54 +0000

    Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,22 +6,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int count = 0;
-
-        while (true) {
-            System.out.println("Give a number:");
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == 0) {
-                System.out.println("Give a number: " + count);
-                System.out.println("Sum of the numbers: " + sum);
-                break;
-            } 
-
-            sum = sum + number;
-            count += 1;
-            
+        Scanner reader = new Scanner(System.in);
+        int number = Integer.valueOf(reader.nextLine());
+        
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
         }
     }
 }
