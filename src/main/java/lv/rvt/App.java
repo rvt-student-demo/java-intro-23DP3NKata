a22bf3993d552c9e248b6bef71a0226a14d8c648
23DP3NKata <166131686+23DP3NKata@users.noreply.github.com>
2024-10-02 06:54:13 +0000

    Repeating, breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,13 +7,24 @@ public class App
     public static void main( String[] args )
     {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Last number ");
-        int n = Integer.valueOf(reader.nextLine());
-        int sum = 0;
-
-        for (int i = 0; i <= n; i++) {
-            sum += i;
+        int sum = 0; int count = 0; int countEven = 0; int countOdd = 0;
+        while (true) {
+            int number = Integer.valueOf(reader.nextLine());
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum: " + sum);
+                System.out.println("Numbers: " + count);
+                System.out.println("Average: " + Double.valueOf(sum)/count);
+                System.out.println("Even: " + countEven);
+                System.out.println("Odd: " + countOdd);
+                break;
+            } 
+            sum = sum + number; count++; 
+            if (number % 2 == 0) {
+                countEven++; 
+            } else if (number % 2 != 0) {
+                countOdd++;
+            }
         }
-        System.out.println("The sum is " + sum);
     }
 }
