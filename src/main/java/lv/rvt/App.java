19aeb14e00cac24a5f1bd82578a8d05671a42bbe
23DP3NKata <166131686+23DP3NKata@users.noreply.github.com>
2024-10-09 07:40:56 +0000

    Index of smallest

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,19 +8,27 @@ public class App {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (int j = 0; j < 10; j++) {
-            numbers.add(j);
-        }
-
-        System.out.println(numbers);
-
-        int smallest = 0;
-        for (int i = 0; i < numbers.size(); i++) {
-            int number = numbers.get(i);
-            if (smallest > number) {
-                smallest = number;
+        while (true) {
+            int number = Integer.valueOf(reader.nextLine());
+            if (number == 9999) {
+                break;
             }
+            numbers.add(number);
         }
-        System.out.println(smallest);
+        System.out.println(numbers);
+        if (numbers.size() > 0) {
+            int smallest = numbers.get(0); int index_of_smallest = 0;
+            for (int i = 1; i < numbers.size(); i++) {
+                int value = numbers.get(i);
+                if (smallest > value) {
+                    smallest = value;
+                    index_of_smallest = i;
+                }
+            }
+            System.out.println("Smallest number: " + smallest);
+            System.out.println("Found at index: " + index_of_smallest);
+        } else {
+            System.out.println("List is empty");
+        }
     }
 }
