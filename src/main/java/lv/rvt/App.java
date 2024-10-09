c3a94ad895d37581d4820de994828da4d0ed6e3a
23DP3NKata <166131686+23DP3NKata@users.noreply.github.com>
2024-10-09 15:38:37 +0000

    Swap

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,24 +2,25 @@ package lv.rvt;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Hello");
-        list.add("World");
-        list.add("!");
-
-        removeLast(list);
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 3;
+        numbers[2] = 5;
+        numbers[3] = 7;
+        numbers[4] = 9;
+        System.out.println(Arrays.toString(numbers));
+        swap(numbers, 2, 4);
+        System.out.println(Arrays.toString(numbers));
     }
 
-    public static void removeLast(ArrayList<String> strings) {
-        if (strings.size() == 0) {
-            return;
-        }
-
-        strings.remove(strings.size() - 1);
-        System.out.println(strings);
+    public static void swap(int[] numbers, int num1, int num2) {
+        int helper = numbers[num1];
+        numbers[num1] = numbers[num2];
+        numbers[num2] = helper;
     }
 }
