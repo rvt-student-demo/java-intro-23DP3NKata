77113afd9725e92f179181f75ef6659300d3e744
23DP3NKata <166131686+23DP3NKata@users.noreply.github.com>
2024-10-09 14:50:55 +0000

    Remove last

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,26 +6,20 @@ import java.util.ArrayList;
 public class App {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(2);
-        list.add(6);
-        list.add(-1);
-        list.add(5);
-        list.add(1);
-        list.add(9);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        list.add("!");
 
-        printNumbersInRange(list, 0, 5);
-        printNumbersInRange(list, 3, 10);
+        removeLast(list);
     }
 
-    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
-        System.out.println("The numbers in the range [" + lowerLimit + ", " + upperLimit + "]");
-        for (int number: numbers) {
-            if (number >= lowerLimit && number <= upperLimit) {
-                System.out.println(number);
-            }
+    public static void removeLast(ArrayList<String> strings) {
+        if (strings.size() == 0) {
+            return;
         }
-        
+
+        strings.remove(strings.size() - 1);
+        System.out.println(strings);
     }
 }
