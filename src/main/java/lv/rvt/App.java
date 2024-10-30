aa137a9c36e3e3303d111d89d8ff4b04f587cd4c
23DP3NKata <166131686+23DP3NKata@users.noreply.github.com>
2024-10-30 07:29:10 +0000

    min, max, average number using list

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,17 +1,33 @@
 package lv.rvt;
+import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
-        Room room = new Room();
-        room.addPerson(new Person("Lea", 183));
-        room.addPerson(new Person("Kenya", 182));
-        room.addPerson(new Person("Auli", 186));
-        room.addPerson(new Person("Nina", 172));
-        room.addPerson(new Person("Terhi", 185));
-        
-        while (!room.isEmpty()) {
-            System.out.println(room.takePerson());
+        Random random = new Random();
+        ArrayList<Integer> randomValue = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            randomValue.add(random.nextInt(10) + 1);
         }
+
+        int minNumber = randomValue.get(0);
+        int maxNumber = randomValue.get(0);
+        int average = 0;
+        for (int i = 0; i < randomValue.size(); i++) {
+            if (randomValue.get(i) < minNumber) {
+                minNumber = randomValue.get(i);
+            }
+
+            if (randomValue.get(i) > maxNumber) {
+                maxNumber = randomValue.get(i);
+            }
+            System.out.print(randomValue.get(i) + ", ");
+            average += randomValue.get(i);
+        }
+        
+        System.out.println();
+        System.out.println(minNumber + " " + maxNumber);
+        System.out.println(Double.valueOf(average/10.0));
+
     }
 }
